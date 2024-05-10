d6b8603bc3f6b3e402da1af381b551e9b1684595
Kayhan <Kayhan@ulu.de>
2024-05-10 16:48:08 +0200

    Demo Supplier

--- lambda-expressions/src/main/java/com/kayulu/Predifined_FI.java
+++ lambda-expressions/src/main/java/com/kayulu/Predifined_FI.java
@@ -1,13 +1,17 @@
 package com.kayulu;
 
+import java.time.LocalTime;
 import java.util.function.BiPredicate;
+import java.util.function.DoubleSupplier;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class Predifined_FI {
     public static void main(String[] args) {
         Predifined_FI pFI = new Predifined_FI();
 
-        pFI.predicate();
+//        pFI.predicate();
+        pFI.supplier();
     }
 
     // Note: a Predicate takes one or more arguments and returns a boolean regarding a
@@ -20,5 +24,19 @@ public class Predifined_FI {
         // a Predicate that takes two arguments
         BiPredicate<String, Integer> biPredicate = (str, len) -> str.length() == len;
         System.out.println(biPredicate.test("City Hall", 10));
+    }
+
+    // Note: A Supplier, as the name suggest takes no args and returns a value
+    public void supplier() {
+        Supplier<String> strSupplier = () -> "Kayhan";
+
+        Supplier<StringBuilder> strBuilder = StringBuilder::new;
+        System.out.println("StringBuilder: " + strBuilder.get().append(strSupplier.get())); // supplier provides
+
+        Supplier<LocalTime> timeSupplier = LocalTime::now;
+        System.out.println("Current time: " + timeSupplier.get());
+
+        DoubleSupplier doubleSupplier = Math::random;
+        System.out.println("Double Supplier: " + doubleSupplier.getAsDouble());
     }
 }
