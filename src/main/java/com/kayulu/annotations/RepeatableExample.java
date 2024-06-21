959bffbd40e3c727435d5f24a1eaaaebf1061aa9
Kayhan <Kayhan@ulu.de>
2024-06-21 14:45:55 +0200

    Demo meta-annotations like Target etc.

---
+++ src/main/java/com/kayulu/annotations/RepeatableExample.java
@@ -0,0 +1,32 @@
+package com.kayulu.annotations;
+
+import java.lang.annotation.Repeatable;
+
+// Define the Container Annotation
+@interface Batteries {      // by convention, the plural of the annotation is used
+    Battery[] value();      // must be value()
+}
+
+// This would be the old way of doing it without requiring the @Repeatable annotation
+//@Batteries({
+//        @Battery(level = "high", recharge = false),
+//        @Battery(level = "low", recharge = true)
+//})
+//class ElectricCar{}
+
+
+// Define the Repeated Annotation
+@Repeatable(Batteries.class)
+@interface Battery{
+    String level();
+    boolean recharge();
+}
+
+@Battery(level="high", recharge=false)
+@Battery(level="low", recharge=true)
+class ElectricCar{}
+
+
+public class RepeatableExample {
+
+}
---
+++ src/main/java/com/kayulu/annotations/RetentionExample.java
@@ -0,0 +1,14 @@
+package com.kayulu.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.SOURCE)  // annotation discarded by the compiler
+@interface Mouse{}                  // i.e. not in .class file
+
+@Retention(RetentionPolicy.RUNTIME) // annotation stored in .class file and
+@interface Keyboard{}               // available at runtime (via reflection)
+
+public class RetentionExample {
+
+}
